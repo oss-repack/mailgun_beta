3a6b3cf5f57f9214cf3307a57e0be5ef494515f2
Stepan Pidlisnyi <spidlisn@gmail.com>
2021-01-27 15:18:50 +0200

    Added LICENSE, setup.py, requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mailgun_beta',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/diskovod/mailgun_beta',
+    license='MIT',
+    author='diskovod',
+    author_email='spidlisn@gmail.com',
+    description='Mailgun Demo'
+)
